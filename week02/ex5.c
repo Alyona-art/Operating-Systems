2b43461ea46ef90be9a3e25e1ecfa6280f69499d
Alyona-art <71446736+Alyona-art@users.noreply.github.com>
2021-08-28 09:23:16 +0300

    Create ex5.c

--- week02/ex5.c
+++ week02/ex5.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #define N 100
-
 void rightSideUpTriangle (int n) {
     for(int i = 0; i <n; i++) {
         for(int j = 0; j < n-i; j++)
@@ -10,7 +9,6 @@ void rightSideUpTriangle (int n) {
         printf("\n");
     }
 }
-
 void rightTriangle (int n) {
     for(int i = 0; i <n; i++) {
         for(int j = 0; j < i + 1; j++)
@@ -18,7 +16,6 @@ void rightTriangle (int n) {
         printf("\n");
     }
 }
-
 void arrow (int n) {
     for(int i = 0; i <n/2; i++) {
         for(int j = 0; j < i +1; j++)
@@ -36,7 +33,6 @@ void arrow (int n) {
         printf("\n");
     }
 }
-
 void square (int n) {
     for(int i = 0; i <n; i++) {
         for(int j = 0; j < n; j++)
@@ -48,29 +44,32 @@ void square (int n) {
 int main() {
     char str[N] = {0};
     int n, x;
-  
     printf("Choose figure by entering a number:\n1) Right-Side-Up Triangle\n2) Right Triangle\n3) Arrow\n4) Square\n");
     scanf("%d", &x);
-    printf("Enter parameter n for creating a figure:\n");
-    scanf("%s", str);
-    sscanf(str, "%d", &n);
+    if (x < 1 || x > 4)
+        printf("Error: wrong value");
+    else {
+        printf("Enter parameter n for creating a figure:\n");
+        scanf("%s", str);
+        sscanf(str, "%d", &n);
 
-    switch (x) {
-        case 1:
-            rightSideUpTriangle(n);
-            break;
+        switch (x) {
+            case 1:
+                rightSideUpTriangle(n);
+                break;
 
-        case 2:
-            rightTriangle(n);
-            break;
+            case 2:
+                rightTriangle(n);
+                break;
 
-        case 3:
-            arrow(n);
-            break;
+            case 3:
+                arrow(n);
+                break;
 
-        case 4:
-            square(n);
-            break;
+            case 4:
+                square(n);
+                break;
+        }
     }
     return 0;
 }
